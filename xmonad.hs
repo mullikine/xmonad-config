ac248f259878e877bc21a60763af368d22f715f6
Shane Mulligan <shane.mulligan@tracmapglobal.com>
2013-08-13 09:39:48 +1200

    fixed inveted xterm by making a delay

--- xmonad.hs
+++ xmonad.hs
@@ -143,7 +143,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     ---- launch bright xterm tmux localhost
     --, ((modm,               xK_i     ), spawn "xterm -ls -rv -e \"resize &> /dev/null; TERM=vt220 tmux attach -t localhost\"")
     -- launch bright xterm tmux localhost
-    , ((modm,               xK_i     ), spawn "bash -c \"set -x; set -m; xterm -ls -e \\\"resize &> /dev/null; TERM=xterm-256color tmux attach -t localhost\\\"& PIDX=\\$!; xprop -id \\$(/home/shane/local/bin/getwid.sh \\$PIDX) -f WM_CLASS 8s -set WM_CLASS 'xtermi';\"")
+    , ((modm,               xK_i     ), spawn "bash -c \"set -x; set -m; xterm -ls -e \\\"resize &> /dev/null; TERM=xterm-256color tmux attach -t localhost\\\"& PIDX=\\$!; sleep 0.1; xprop -id \\$(/home/shane/local/bin/getwid.sh \\$PIDX) -f WM_CLASS 8s -set WM_CLASS 'xtermi';\"")
 
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
