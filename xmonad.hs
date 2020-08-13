4e170013be3e34bf4c9540d1e943113c6d842a83
Shane Mulligan <mullikine@gmail.com>
2020-08-13 23:37:55 +1200

    Thu 13 Aug 2020 23:37:55 NZST

--- xmonad.hs
+++ xmonad.hs
@@ -369,9 +369,9 @@ myBar = "xmobar"
 -- ppCurrent 2C882C
 -- ppTitle 2C882C
 
-myPP = xmobarPP { ppCurrent = xmobarColor "#ff9999" ""
+myPP = xmobarPP { ppCurrent = xmobarColor "#444444" ""
                 , ppSep = " "
-                , ppTitle   = xmobarColor "#ff6666"  "" . shorten 1000
+                , ppTitle   = xmobarColor "#333333"  "" . shorten 1000
                 , ppOrder   = \(ws:_:t:_)   -> [ws,t] }
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
